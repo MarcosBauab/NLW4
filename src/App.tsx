48c8997276d897c0d30651e1e2305b9c0ebb45a2
Marcos Vinicius Gasparoto Bauab <marcosbauab@gmail.com>
2021-02-24 15:26:12 -0300

    24/02
    -Primeiro dia NLW completo

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
-import { Button } from './components/Button'
-
 import './styles/global.css';
-
+import { ExperienceBar } from './components/ExperienceBar';
 
 function App() {
   return (
-    <div>
-      <Button color="red">
-        Botão 1
-      </Button>
-      <Button color="green">
-        Botão 2
-      </Button>
-      <Button color="blue">
-        Botão 3
-      </Button>
+    <div className="container">
+      <ExperienceBar/>
     </div>
   );
 }
